4782107520aa7116d2f7aa235929cb1a17881719
POUPAERT Vincent <baidem@gmail.com>
2022-11-29 21:59:07 +0100

    Entities Modifications
    - Modified : Post.cs
    - Modified : Comment.cs

--- Entities/Comment.cs
+++ Entities/Comment.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,13 +11,14 @@ namespace Entities
     public class Comment
     {
         public int Id { get; set; }
-        [Required]
         public DateTime CreatedDate { get; set; }
         public DateTime EditedDate { get; set; }
         [Required]
-        public int AuthorId { get; set; }
-        public User Author { get; set; }
+        [Column("AuthorId")]
+        public int UserId { get; set; }
+        public User User { get; set; }
         [Required]
+        [StringLength(100)]
         public string Content { get; set; }
         public int PostId { get; set; }
         public Post Post { get; set; }
--- Entities/Post.cs
+++ Entities/Post.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,14 +11,16 @@ namespace Entities
     public class Post
     {
         public int Id { get; set; }
-        public string Theme { get; set; }
-        public User Author { get; set; }
+        public string Title { get; set; }
+        [Required]
+        [Column("AuthorId")]
+        public int UserId { get; set; }
+        public User User { get; set; }
         public DateTime CreatedDate { get; set; }
         public DateTime EditedDate { get; set; }
         public string Content { get; set; }
         public List<Comment> Comments { get; set; }
         public string Image { get; set; }
-        public User User { get; set; }
 
     }   
 }
