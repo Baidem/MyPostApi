643df8b1ef4ec65b8346b0840966e9191c38c981
POUPAERT Vincent <baidem@gmail.com>
2022-11-29 18:34:27 +0100

    Context OnConfiguring
    - Added in MyPostAPIContext  : OnConfiguring
    - Memo string connection : optionsBuilder.UseSqlServer(@"Server=(localdb)\\mssqlloocaldb;Database=MyPostApi;Trusted_Connection=True;");

--- Repositories/MyPostApiContext.cs
+++ Repositories/MyPostApiContext.cs
@@ -13,5 +13,15 @@ namespace Repositories
         public DbSet<Post> Posts { get; set; }
         public DbSet<Comment> Comments { get; set; }
         public DbSet<User> Users { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer(@"Server=(localdb)\\mssqlloocaldb;Database=MyPostApi;Trusted_Connection=True;");
+            }
+
+            base.OnConfiguring(optionsBuilder);
+        }
     }
 }
