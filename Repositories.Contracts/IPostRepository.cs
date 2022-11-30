085950b8aefc5455d9946797d69127baac584cd8
POUPAERT Vincent <baidem@gmail.com>
2022-11-30 13:56:47 +0100

    Add IPostRepository.cs
    - Methods : GetAllPostsAsync, GetPostAsync, GetPostWithCommentsAsync, AddPostAsync
    - Update : Repositories.Contracts.csproj

---
+++ Repositories.Contracts/IPostRepository.cs
@@ -0,0 +1,17 @@
+﻿using Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repositories.Contracts
+{
+    public interface IPostRepository
+    {
+        Task<List<Post>> GetAllPostsAsync();
+        Task<Post?> GetPostAsync(int id);
+        Task<Post?> GetPostWithCommentsAsync(int id);
+        Task<Post> AddPostAsync(Post post);
+    }
+}
