2090c7c8d5bd90c047af6a39d0b57530993ef3fe
POUPAERT Vincent <baidem@gmail.com>
2022-11-30 21:47:04 +0100

    Modification Comment.cs
    - Modif nullables

--- Entities/Comment.cs
+++ Entities/Comment.cs
@@ -11,16 +11,16 @@ namespace Entities
     public class Comment
     {
         public int Id { get; set; }
-        public DateTime CreatedDate { get; set; }
-        public DateTime EditedDate { get; set; }
+        public DateTime? CreatedDate { get; set; }
+        public DateTime? EditedDate { get; set; }
         [Column("AuthorId")]
         public int? UserId { get; set; }
-        public User User { get; set; }
+        public User? User { get; set; }
         [Required]
         [StringLength(100)]
         public string Content { get; set; }
-        public int PostId { get; set; }
-        public Post Post { get; set; }
+        public int? PostId { get; set; }
+        public Post? Post { get; set; }
 
 
     }
