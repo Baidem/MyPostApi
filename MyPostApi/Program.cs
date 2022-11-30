ea278f04b6ab1698456834fdc1d424b0e3906dd5
POUPAERT Vincent <baidem@gmail.com>
2022-11-30 14:40:39 +0100

    Move builder.Services.AddDbContext
    - Moved from MyPostApiContext to program.cs
    - Added to appsettings.json the ConnectionStrings "MyPostApiDbCS"
    - Added : mode debug : "o.LogTo(Console.Write);"

--- MyPostApi/Program.cs
+++ MyPostApi/Program.cs
@@ -2,6 +2,8 @@ using Repositories;
 using Entities;
 using Repositories.Contracts;
 using System.Text.Json.Serialization;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -11,6 +13,25 @@ builder.Services.AddControllers().AddJsonOptions(o =>
 {  // Fix Include(p => p.Comments) : Error 500 "...JsonSerializerOptions to support cycles...
     o.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles;
 });
+
+builder.Services.AddDbContext<MyPostApiContext>(o =>
+{
+    o.UseSqlServer(builder.Configuration.GetConnectionString("MyPostApiDbCS"));
+
+    // mode debug
+    //o.LogTo(Console.Write);
+});
+
+// Forcer les migrations en attentes (évite de faire le update-database)
+//using (var serviceScope = app.Services.CreateScope())
+//{
+//    var services = serviceScope.ServiceProvider;
+//    var wikyContext = services.GetRequiredService<WikyContext>();
+//    wikyContext.Database.Migrate();
+
+//    //wikyContext.Database.EnsureDeleted(); // Efface la Db
+//    //wikyContext.Database.EnsureCreated(); // Recrer la Db sans prendre en compte les migrations
+//}
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
