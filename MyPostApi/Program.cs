25ddd585f1cfb2c42ce013733f07a7596dee7430
POUPAERT Vincent <baidem@gmail.com>
2022-11-30 13:48:16 +0100

    Program.cs
    - Fix : migration Init : added "builder.Services.AddDbContext<MyPostApiContext>();"
    - Fix : PostController : added "builder.Services.AddScoped<IPostRepository, PostRepository>();"
    - Fix : include(p => p.Comments) => Error 500 "...JsonSerializerOptions to support cycles... : added "builder.Services.AddControllers().AddJsonOptions(o =>..."

--- MyPostApi/Program.cs
+++ MyPostApi/Program.cs
@@ -1,15 +1,27 @@
 using Repositories;
 using Entities;
+using Repositories.Contracts;
+using System.Text.Json.Serialization;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+builder.Services.AddControllers().AddJsonOptions(o =>
+{  // Fix Include(p => p.Comments) : Error 500 "...JsonSerializerOptions to support cycles...
+    o.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles;
+});
+
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddDbContext<MyPostApiContext>();
+builder.Services.AddDbContext<MyPostApiContext>(); // Fix migration Init
+
+builder.Services.AddDbContext<MyPostApiContext>(); // Fix PostController
+
+builder.Services.AddScoped<IPostRepository, PostRepository>(); // Fix PostController
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
