5c77a5788a0a50f2448d77b0ed94ce128862f61f
POUPAERT Vincent <baidem@gmail.com>
2022-12-03 17:57:03 +0100

    Dto Created

---
+++ Dto/PostDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dto
+{
+    public class PostDto
+    {
+        public string Title { get; set; } = string.Empty;
+        public string Theme { get; set; } = string.Empty;
+        public string Content { get; set; } = string.Empty;
+    }
+}
